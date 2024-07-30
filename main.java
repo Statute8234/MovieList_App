5f5d1f30631f623c9c27a8086a2407ce4c8ef525
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-07-30 10:37:01 -0400

    Update main.java
    _ remove app theme

--- main.java
+++ main.java
@@ -4,13 +4,6 @@ import java.util.HashMap;
 
 public class main {
     public static void main(String[] args) {
-
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         Authorization authorization_data = new Authorization();
         LoginForm loginPage = new LoginForm(authorization_data.getLoginInfo());
     }
