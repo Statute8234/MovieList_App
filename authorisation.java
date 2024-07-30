f74b9caf4820a7b95d43dd21adfac0feeff5d6b6
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-07-30 10:30:24 -0400

    Update authorisation.java
    _ adding user info database
    _ checking if users exists
    _ checking if users email exists

--- authorisation.java
+++ authorisation.java
@@ -1,14 +1,64 @@
 import java.util.HashMap;
 
-public class authorisation {
-    HashMap<String, String> loginInfo = new HashMap<String, String>();
-
-    authorisation() {
-        loginInfo.put("admin", "Wb6co8S5yom@");
-        loginInfo.put("robert smoooth man","human very smooth");
+public class Authorization {
+    private HashMap<String, User> userInfo;
+    
+    Authorization() {
+        userInfo = new HashMap<>();
+        userInfo.put("Username", new User("Username", "Password", "email", "Full Name"));
     }
 
     protected HashMap getLoginInfo() {
-        return loginInfo;
+        return userInfo;
+    }
+
+    public boolean verifyUser(String username, String password) {
+        User user = userInfo.get(username);
+        return user != null && user.getPassword().equals(password);
+    }
+
+
+    public void addUser(String username, String password, String email, String fullName) {
+        userInfo.put(username, new User(username, password, email, fullName));
+    }
+
+    public boolean userExists(String username) {
+        return userInfo.containsKey(username);
+    }
+
+    public boolean emailExists(String email) {
+        for (User user : userInfo.values()) {
+            if (user.getEmail().equalsIgnoreCase(email)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public class User {
+        private String username;
+        private String password;
+        private String email;
+        private String fullName;
+    
+        public User(String username, String password, String email, String fullName) {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+            this.fullName = fullName;
+        }
+    
+        public String getPassword() {
+            return password;
+        }
+    
+        public String getEmail() {
+            return email;
+        }
+    
+        public String getFullName() {
+            return fullName;
+        }
     }
 }
